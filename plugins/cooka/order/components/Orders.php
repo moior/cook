52d9da418343b266e609f6f2b8e1fab5ac2651ea
moior <help@moior.com>
2017-03-03 16:13:50 +0900

    .

--- plugins/cooka/order/components/Orders.php
+++ plugins/cooka/order/components/Orders.php
@@ -72,64 +72,16 @@ class Orders extends ComponentBase
     /*접수버튼 클릭시*/
     public function onSaveOrder()
     {
-        //$items = post('items', []); // 원래있는것. 배열처리방법.
-        /*if (!count($items)) {
-            throw new ApplicationException(sprintf('뭔가 이상 : %s ', $this->property('type')));
-        }*/
-        //$user = BackendAuth::getUser(); //$user["email"]. $user->name; //
-        $user = Auth::getUser(); //$user["email"]. $user->name; //
 
-        /*만약 로긴 아니면, guest 모델 만들어서 가입할까......*/
-        /*$user = $this->user;*/
-        $order = new Order; /*Db::table('users')->insert(
-                                ['email' => 'john@example.com', 'votes' => 0]
-                                );*/
         $form = post();
-        $order->name        = isset($form['client_name'])?$form['client_name']:"";
-        if( isset($user->id)){
-            $order->user_id     = $user->id;
-        }else{
-            $order->user_id     = "0";
-        }
-        $order->title           = isset($form["title"])?$form["title"]:"";
-        $order->phone           = isset($form["client_tel"])?$form["client_tel"]:"";
-        $order->addr            = isset($form["client_addr"])?$form["client_addr"]:"";
-        $order->email           = isset($form["client_email"])?$form["client_email"]:"";
-        $order->comment         = isset($form["client_comment"])?$form["client_comment"]:"";
-        $order->sample_code     = isset($form["sample_code"])?$form["sample_code"]." ".$form["sample_name"]:"";
+        $order = $this->saveOrder($form);
 
-        //$order->wishlist_id = form[""];
-        $order->cook_data       = isset($form["cook_data"])?$form["cook_data"]:""; // yml 으로 변환 //$parsed = yaml_parse($yaml);        // convert the YAML back into a PHP variable
-        if(Input::hasFile('upload_file')){//Request::hasFile(
-            $order->upload_file     = Input::file('upload_file'); //$form["upload_file"]; //
-        }
-        $order->bill            = isset($form["bill"])?$form["bill"]:"";
-        $order->fee_offer       = isset($form["fee_offer"])?$form["fee_offer"]:"0";
-        //$order->fee_payment     = $form[""];
-        $order->status_show     = isset($form["status_show"])?$form["status_show"]:"접수확인중";
-        $order->shipping_number = "";
-        //$order->is_complete     = "";
+        $this->sendMailAfterOrder($order, "cooka.order::mail.ordered");
+        $this->sendSMSAfterOrder($order,
+            '노트요리사::주문완료되었습니다. 확인 후 연락 드리겠습니다. 노트요리사.com');
+            /*======================80자======================================================?? */
 
-        $order->save();
-
-        if( $order->email){
-            $data = [
-                'bill' => $order->bill,
-                'name' => $order->name,
-            ];
-            Mail::send('cooka.order::mail.ordered', $data, function($message) use ($order) {
-                $message->to($order->email, $order->name);
-            });
-        }
-
-        if($order->phone){
-            $ret = \Kenny\Sms\Components\SmsController::send([
-                /*======================80자======================================================?? */
-                'text'=>'노트요리사::주문완료되었습니다. 확인 후 연락 드리겠습니다. 노트요리사.com',
-                'from' => '02-1661-5521',
-                'to' => $order->phone
-            ]);
-        }
+        /*우리 요리사들에게 문자*/
         $ret = \Kenny\Sms\Components\SmsController::send([
             'text'=>'노트요리사::주문접수됨! '. $order->name. '/' . $order->phone. '/'
                 .$order->title. '/'. $order->fee_offer,
@@ -137,27 +89,34 @@ class Orders extends ComponentBase
             'to' => '010-4775-0852, 010-4618-7725',
         ]); /*김상겸, 장미라에게 문자*/
 
-        return true;
-
     }
 
-    /*접수버튼 클릭시*/
+
+    /*전화받아서 상담입력시*/
     public function onSaveOrderAdmin()
     {
-        //$items = post('items', []); // 원래있는것. 배열처리방법.
-        /*if (!count($items)) {
-            throw new ApplicationException(sprintf('뭔가 이상 : %s ', $this->property('type')));
-        }*/
+        $form = post();
+        $order = $this->saveOrder($form);
+
+        /*의뢰인께 내용 알릴려면*/
+        if(isset($form["send_email"]) && $form["send_email"] == true){
+            $this->sendMailAfterOrder($order, "cooka.order::mail.thanksForContact");
+        }
+        if(isset($form["send_sms"]) && $form["send_sms"] == true) {
+            if( !isset($form["send_sms_text"])){
+                $form["send_sms_text"] = "[직접 만드는 나만의 노트, 노트요리사.com] 상담이 접수되었습니다 감사합니다.";
+            }
+            $this->sendSMSAfterOrder($order, $form["send_sms_text"]);
+        }
+    }
+
+    /*DB에 저장 내부함수*/
+    private function saveOrder($form){
+        $order = new Order;
+        $order->name        = isset($form['client_name'])?$form['client_name']:"";
+
         //$user = BackendAuth::getUser(); //$user["email"]. $user->name; //
         $user = Auth::getUser(); //$user["email"]. $user->name; //
-
-        /*만약 로긴 아니면, guest 모델 만들어서 가입할까......*/
-        /*$user = $this->user;*/
-        $order = new Order; /*Db::table('users')->insert(
-                                ['email' => 'john@example.com', 'votes' => 0]
-                                );*/
-        $form = post();
-        $order->name        = isset($form['client_name'])?$form['client_name']:"";
         if( isset($user->id)){
             $order->user_id     = $user->id;
         }else{
@@ -170,7 +129,6 @@ class Orders extends ComponentBase
         $order->comment         = isset($form["client_comment"])?$form["client_comment"]:"";
         $order->sample_code     = isset($form["sample_code"])?$form["sample_code"]." ".$form["sample_name"]:"";
 
-        //$order->wishlist_id = form[""];
         $order->cook_data       = isset($form["cook_data"])?$form["cook_data"]:""; // yml 으로 변환 //$parsed = yaml_parse($yaml);        // convert the YAML back into a PHP variable
         if(Input::hasFile('upload_file')){//Request::hasFile(
             $order->upload_file     = Input::file('upload_file'); //$form["upload_file"]; //
@@ -180,36 +138,36 @@ class Orders extends ComponentBase
         //$order->fee_payment     = $form[""];
         $order->status_show     = isset($form["status_show"])?$form["status_show"]:"접수확인중";
         $order->shipping_number = "";
-        //$order->is_complete     = "";
 
         $order->save();
+        return $order;
+    }
 
-        if( $order->email){
+    private function sendMailAfterOrder($order, $mail_view = "cooka.order::mail.ordered"){
+        if( $order->email ){
             $data = [
                 'bill' => $order->bill,
                 'name' => $order->name,
             ];
-            Mail::send('cooka.order::mail.ordered', $data, function($message) use ($order) {
+            Mail::send($mail_view, $data, function($message) use ($order) {
                 $message->to($order->email, $order->name);
             });
         }
-
-        if($order->phone){
+    }
+    private function sendSMSAfterOrder($order, $sms_text = "[노트요리사] 상담이 접수되었습니다. 노트요리사.com")
+    {
+        if ($order->phone) {
             $ret = \Kenny\Sms\Components\SmsController::send([
                 /*======================80자======================================================?? */
-                'text'=>'노트요리사::상담이 접수되었습니다. 노트요리사.com',
+                'text' => $sms_text,
                 'from' => '02-1661-5521',
                 'to' => $order->phone
             ]);
         }
-        $ret = \Kenny\Sms\Components\SmsController::send([
-            'text'=>'노트요리사::주문접수됨! '. $order->name. '/' . $order->phone. '/'
-                .$order->title. '/'. $order->fee_offer,
-            'from' => '02-1661-5521',
-            'to' => '010-4775-0852, 010-4618-7725',
-        ]); /*김상겸, 장미라에게 문자*/
-
-        return true;
-
     }
 }
+//$items = post('items', []); // 원래있는것. 배열처리방법.
+/*if (!count($items)) {
+    throw new ApplicationException(sprintf('뭔가 이상 : %s ', $this->property('type')));
+}*/
+//$user = BackendAuth::getUser(); //$user["email"]. $user->name; //
